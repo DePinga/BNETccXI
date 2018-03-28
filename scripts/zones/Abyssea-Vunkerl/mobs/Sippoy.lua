c7ab6c1e25a0bfe6f9b6f84e12b79db7eef25616
Ninji <ninji418@gmail.com>
2018-03-28 13:36:00 -0400

    Abyssea-Vunkerl prep

--- scripts/zones/Abyssea-Vunkerl/mobs/Sippoy.lua
+++ scripts/zones/Abyssea-Vunkerl/mobs/Sippoy.lua
@@ -1,7 +1,12 @@
 -----------------------------------
--- Area: Abyssea - Vunkerl
+-- Area: Abyssea - Vunkerl (217)
 --  Mob: Sippoy
 -----------------------------------
+package.loaded["scripts/zones/Abyssea-Vunkerl/textIDs"] = nil;
+-----------------------------------
+require("scripts/zones/Abyssea-Vunkerl/textIDs");
+require("scripts/globals/keyitems");
+require("scripts/globals/abyssea");
 require("scripts/globals/status");
 require("scripts/globals/titles");
 -----------------------------------
@@ -10,6 +15,7 @@ function onMobInitialize(mob)
 end;
 
 function onMobSpawn(mob)
+
 end;
 
 function onMobEngaged(mob,target)
@@ -27,4 +33,28 @@ end;
 
 function onMobDeath(mob, player, isKiller)
     player:addTitle(SIPPOY_CAPTURER);
+    --[[
+    if (isKiller == true) then
+        local itemRate = math.random(1,100);
+        local lootTable =
+        {
+            [1] = itemID, -- comment me
+            [2] = itemID, -- comment me
+            [3] = itemID, -- comment me
+            [4] = itemID, -- comment me
+            [5] = itemID  -- comment me
+        }
+        if (itemRate >= 50) then -- First drop is 50 in 100.
+            player:addTreasure(lootTable[math.random(1,5)], mob);
+        end
+        if (itemRate >= 90) then -- You lucky high roller, 2nd drop is only 10 in 100
+            player:addTreasure(lootTable[math.random(1,5)], mob);
+        end
+    end
+    ]]
+    local CHANCE = 15;
+    if (math.random(0,99) < CHANCE  and player:hasKeyItem(ATMA_OF_THE_WOULD_BE_KING) == false) then
+        player:addKeyItem(ATMA_OF_THE_WOULD_BE_KING);
+        player:messageSpecial(KEYITEM_OBTAINED, ATMA_OF_THE_WOULD_BE_KING);
+    end
 end;
